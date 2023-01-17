261b02b45d1ae88dbd3933087d4136525f937cc1
Aram Weber <tidus.zakk@gmail.com>
2023-01-17 17:26:10 +0100

    added Colorgen. Prolem "randomColor" not found

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hallo');
+const randomColor = require('randomColor');
+const chalk = require('chalk');
+const color = randomColor({
+  hue: process.argv[2],
+  luminosity: process.argv[3],
+});
+
+const output = chalk.hex(color)`
+#########################
+#########################
+#########################
+#####               #####
+#####    ${color}    #####
+#####               #####
+#########################
+#########################
+#########################`;
+
+console.log(output);
